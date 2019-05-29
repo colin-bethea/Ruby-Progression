d3e3bfda6dc1861e3b9c13400765bd227b25664c
Colin Bethea <colinjbethea@gmail.com>
2019-05-29 18:30:07 -0400

    Add ex31 and ex32

---
+++ ex32.rb
@@ -0,0 +1,31 @@
+count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# Traditional style for-loop
+for number in count
+  puts "This is count #{number}"
+end
+
+# Ruby style for-loop
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# Mixed style for-loop
+change.each {|i| puts "I got #{i}"}
+
+# Starting with an empty list
+elements = []
+
+# Using range operator to do counts (again)
+
+final_count = (0..5)
+
+final_count.each do |i|
+  puts "Adding #{i} to the list."
+  # (.push) pushes the i var on the end of the list
+  elements.push(i)
+end
+
+elements.each {|i| puts "Element was #{i}"} # Prints Element was [x] for each element
